808ef48d44170a2e8d452b0ac9b22efa6d0bab52
Roberto <Roberto>
2013-03-15 08:34:49 +0100

    Changeset: 61 Added service class.

---
+++ Metrics/ViewModel/Service.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Metrics.ViewModel
+{
+    public class Service
+    {
+        public string MetricsName { get; set; }
+        public string MetricsProvider { get; set; }
+    }
+}
